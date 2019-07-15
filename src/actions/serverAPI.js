657327ba1aaa7b298175cc1aa8103b55e2c93db7
Emmanuel <mygitmail@yahoo.com>
2019-07-15 12:23:43 +0100

    chore[#167287726]: Setup Redux
    - setup redux environment
    - setup webpack
    - install application dependencies
    
    [Finishes #167287726]

--- src/actions/serverAPI.js
+++ src/actions/serverAPI.js
@@ -1,8 +1,4 @@
 
 let backendAPI = 'https://dry-journey-38911.herokuapp.com/api/v1';
 
-// if(window.location.href.includes('localhost')) {
-//     backendAPI = 'http://localhost:7000/api/v1';
-// }
-
 export default backendAPI;
